42801e1b7fe8f11afd0e9cea244f08a001dbb308
arwahab <arw9511@gmail.com>
2019-08-04 09:52:41 -0400

    routing with error handling added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/restaurants', pathMatch: 'full' },
   {path: 'restaurants', component: RestaurantListComponent},
-  {path: 'foods', component: FoodListComponent}
+  {path: 'foods', component: FoodListComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -13,4 +16,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [RestaurantListComponent, FoodListComponent]
+export const routingComponents = [RestaurantListComponent, 
+                                  FoodListComponent,
+                                  PageNotFoundComponent]
